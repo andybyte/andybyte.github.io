e61ff2beadf8351487a6a34495e5e25f9af1735f
Andy <ajmrkva@gmail.com>
2021-07-16 19:47:53 -0400

    version 2 of website

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
-        "title": "Report B",
+        "title": "Hello Andy!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["andy","mrkva"],
         "tags": [],
         "url": "/andy/mrkva/2021/06/20/hello-andy-mrkva.html",
         "teaser": null
       },{
-        "title": "Report A",
+        "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
         "url": "/jekyll/update/2021/06/24/welcome-to-jekyll.html",
